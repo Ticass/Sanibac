8dedafa685aa957bf4a868d28c420dbaf7789dd2
Alexis Grenier <thrifthshopdowntheroad@gmail.com>
2021-08-12 14:48:48 -0400

    Lancement de Sanibac :tada:

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Header></Header>
+  <Pricing
+  description1="bronze" description2="argent" description3="or" saison="1er avril 2020 au 30 novembre 2022" slogan="Lavage de bacs à domicile"></Pricing>
+  
+  
+  
+</template>
+
+<script setup>
+import Pricing from './components/Pricing.vue'
+import Header from './components/Header.vue'
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

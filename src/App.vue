6d2091b3d8cae4302845076d43e779add56fd22f
Alexis Grenier <thrifthshopdowntheroad@gmail.com>
2021-08-12 15:41:53 -0400

    :lipstick: Added basic UI

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <Header></Header>
+  <Hero></Hero>
+  <Gallery></Gallery>
   <Pricing
   description1="bronze" description2="argent" description3="or" saison="1er avril 2020 au 30 novembre 2022" slogan="Lavage de bacs à domicile"></Pricing>
-  
+  <Steps></Steps>
+  <Form></Form>
+  <Footer></Footer>
   
   
 </template>
@@ -10,6 +14,11 @@
 <script setup>
 import Pricing from './components/Pricing.vue'
 import Header from './components/Header.vue'
+import Hero from './components/Hero.vue'
+import Gallery from './components/Gallery.vue'
+import Steps from './components/Steps.vue'
+import Footer from './components/Footer.vue'
+import Form from './components/Form.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md

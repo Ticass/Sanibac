bf4ec8737b5f172971ad3fdbba430b04e7901729
ricoloic <ricoloic4@gmail.com>
2022-01-06 21:10:27 -0500

    added card hoverring

--- src/components/rico-components/Comparation.vue
+++ src/components/rico-components/Comparation.vue
@@ -1,18 +1,39 @@
 <template>
 
   <section id="comparation" class="flex justify-evenly my-12 mx-40">
-    <div id="before" class="">
-      <h2 class="text-center font-bold text-2xl">
-        Avant
+    <div id="before" class="w-1/3">
+      <h2 class="text-center text-3xl mb-5 underline">
+        Avant notre passage
       </h2>
-      <img alt="avant netoyage" src="../../assets/bac.jpeg" />
+      <div class="outter">
+        <div class="inner" />
+        <img class="w-full" alt="avant netoyage" src="../../assets/bac.jpeg" />
+      </div>
     </div>
-    <div id="after" class="">
-      <h2 class="text-center font-bold text-2xl">
-        Avant
+    <div id="after" class="w-1/3">
+      <h2 class="text-center text-3xl mb-5 underline">
+        Apres notre passage
       </h2>
-      <img alt="avant netoyage" src="../../assets/bac2.jpeg" />
+      <div class="outter">
+        <div class="inner" />
+        <img class="w-full" alt="avant netoyage" src="../../assets/bac2.jpeg" />
+      </div>
     </div>
   </section>
 
 </template>
+
+<style scoped>
+  .outter {
+    position: relative;
+  }
+  .inner {
+    position: absolute;
+    top: 22px;
+    left: 12px;
+    width: 100%;
+    height: 100%;
+    border: 2px solid #000;
+    z-index: -1;
+  }
+</style>
---
+++ src/components/rico-components/Pricing.vue
@@ -0,0 +1,185 @@
+<template>
+<section class="text-gray-600 body-font overflow-hidden">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-col text-center w-full mb-20">
+      <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">{{prix}}</h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-500">{{prixDescription}}</p>
+        <p class="py-1 px-4 bg-green-500 text-white focus:outline-none">{{saison}}</p>
+    </div>
+    <div class="flex flex-wrap -m-4">
+      <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
+        <div v-bind:class="[card]">
+          <h2 class="text-sm tracking-widest title-font mb-1 font-medium">{{option1}}</h2>
+          <h1 class="text-5xl text-gray-900 pb-4 mb-4 border-b border-gray-200 leading-none">{{priceOption1}}</h1>
+          <p class="flex items-center text-gray-600 mb-2">
+            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>{{Option1description1}}
+          </p>
+          <p class="flex items-center text-gray-600 mb-2">
+            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>{{Option1description2}}
+          </p>
+          <p class="flex items-center text-gray-600 mb-6">
+            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>{{Option1description3}}
+          </p>
+          <button v-bind:class="[button]">{{Signup}}
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
+        <div v-bind:class="[card]">
+          <span class="bg-green-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">OFFRE AVANTAGEUSE</span>
+          <h2 class="text-sm tracking-widest title-font mb-1 font-medium">{{option2}}</h2>
+          <h1 class="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
+            <span>{{priceOption2}}</span>
+            <span class="text-lg ml-1 font-normal text-gray-500">/mo</span>
+          </h1>
+          <p class="flex items-center text-gray-600 mb-2">
+            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>{{Option2description1}}
+          </p>
+          <p class="flex items-center text-gray-600 mb-2">
+            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>{{Option2description2}}
+          </p>
+          <p class="flex items-center text-gray-600 mb-2">
+            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>{{Option2description3}}
+          </p>
+          <button class="flex items-center mt-auto text-white bg-green-500 border-0 py-2 px-4 w-full focus:outline-none hover:bg-green-600 rounded">{{Signup}}
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
+        <div v-bind:class="[card]">
+          <h2 class="text-sm tracking-widest title-font mb-1 font-medium">{{option3}}</h2>
+          <h1 class="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
+            <span>{{priceOption3}}</span>
+            <span class="text-lg ml-1 font-normal text-gray-500">/mo</span>
+          </h1>
+          <p class="flex items-center text-gray-600 mb-2">
+            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>{{Option3description1}}
+          </p>
+          <p class="flex items-center text-gray-600 mb-2">
+            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>{{Option3description2}}
+          </p>
+          <p class="flex items-center text-gray-600 mb-2">
+            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>{{Option3description3}}
+          </p>
+          <button v-bind:class="[button]">{{Signup}}
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
+        <div v-bind:class="[card]">
+          <h2 class="text-sm tracking-widest title-font mb-1 font-medium">{{option4}}</h2>
+         <h1 class="text-5xl text-gray-900 pb-4 mb-4 border-b border-gray-200 leading-none">{{priceOption4}}</h1>
+          <p class="flex items-center text-gray-600 mb-2">
+            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>{{Option4description1}}
+          </p>
+          <p class="flex items-center text-gray-600 mb-2">
+            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>{{Option4description2}}
+          </p>
+          <p class="flex items-center text-gray-600 mb-2">
+            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>{{Option4description3}}
+          </p>
+          <button v-bind:class="[button]">{{Signup}}
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+const card = "group h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden hover:border-green-500"
+const button = "flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none group-hover:bg-green-500 rounded"
+
+defineProps({
+    Option1description1: String,
+    Option1description2: String,
+    Option1description3: String,
+    Option2description1: String,
+    Option2description2: String,
+    Option2description3: String,
+    Option3description1: String,
+    Option3description2: String,
+    Option3description3: String,
+    Option4description1: String,
+    Option4description2: String,
+    Option4description3: String,
+    saison: String,
+    slogan: String,
+    prix: String,
+    prixDescription: String,
+    priceOption1: String,
+    option1: String,
+    option2: String,
+    option3: String,
+    priceOption2: String,
+    priceOption3: String,
+    option4: String,
+    priceOption4: String,
+    Signup: String,
+
+})
+
+
+</script>
